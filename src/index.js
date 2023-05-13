ddbf0e3bb453e1dc19987e7e97c37b5ecf6a3da7
osamabalghari <96984920+osamabalghari@users.noreply.github.com>
2023-05-13 13:38:54 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -35,8 +35,11 @@ const autoCreate = async () => {
   }
 };
 autoCreate();
+const corsOptions = {
+  origin: process.env.FRONTEND_LINK,
+};
 
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(express.json({ limit: "50mb", extended: true }));
 server.use(
   express.urlencoded({ limit: "50mb", parameterLimit: 1000000, extended: true })
